3c6a002f6681a359c851704f3c03b72529b073d0
Voldemat <77781574+Voldemat@users.noreply.github.com>
2021-03-21 20:38:36 +0300

    'add_serializers_and_generic_views'

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,42 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from .models import Poll
+from .models import Poll, Choice
+from .serializers import (
+    PollSerializer,
+    ChoiceSerializer,
+    VoteSerializer
+)
 
-def polls_list(request):
-    max_objects = 20
-    polls = Poll.objects.all()[:max_objects]
-    data = {"result":list(polls.values("question","created_by__username", "pub_date"))}
-    return JsonResponse(data, status = 200)
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
-def polls_detail(request, pk):
-    poll = get_object_or_404(Poll, pk = pk)
-    data = {
-        "results":{
-            "question":poll.question,
-            "created_by":poll.created_by.username,
-            "pub_date":poll.pub_date,
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id = self.kwargs["pk"])
+        return queryset
+    serializer_class = ChoiceSerializer
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {
+            "choice":choice_pk,
+            "poll":pk,
+            "voted_by": voted_by
         }
-    }
-    return JsonResponse(data, status = 200)
+        serializer = VoteSerializer(data = data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
